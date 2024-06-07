6f17e59cf7c54af4cfbece5c25fa8bb405d15848
carodak <carodak2@gmail.com>
2024-06-07 16:23:11 -0400

    MRI Bed sliding effect + Reset position button changed the position

---
+++ Assets/Scenes/MRIRoom/Scripts/DragXRRig.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class DragXRRig : MonoBehaviour
+{
+    [SerializeField] private Transform xRRigTransform;
+    [SerializeField] private float dragSpeed = 1.0f;
+
+    private Vector3 previousPosition;
+    private bool isDragging = false;
+
+    void Start()
+    {
+        previousPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (!isDragging && transform.position != previousPosition)
+        {
+            PerformDragging();
+        }
+    }
+
+    void PerformDragging()
+    {
+        Debug.Log("Perform Dragging..");
+        previousPosition = transform.position;
+        isDragging = true;
+        StartCoroutine(DoDragging());
+    }
+
+    IEnumerator DoDragging()
+    {
+        Debug.Log("Do Dragging..");
+        while (isDragging)
+        {
+            Vector3 currentPosition = transform.position;
+            Vector3 movement = currentPosition - previousPosition;
+            xRRigTransform.position += movement;
+            Debug.Log("XRRig pos: " + xRRigTransform.position);
+
+            previousPosition = currentPosition;
+
+            yield return null;
+        }
+
+        isDragging = false;
+    }
+}
