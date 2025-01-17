3a0bb75d23880bd913f85f084890bac75f1ef95d
carodak <carodak2@gmail.com>
2025-01-17 17:42:21 -0500

    Fixed typo + seq, to fix: Moon landing loop seq+fade to landing

--- Assets/Scenes/MRIRoom/Scripts/PlaySoundLoopSimple.cs
+++ Assets/Scenes/MRIRoom/Scripts/PlaySoundLoopSimple.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class PlaySoundLoopSimple : MonoBehaviour
 {
-    private AudioSource audioSource;
+    public AudioSource audioSource;
 
     public void Start()
     {
         // Add an AudioSource component to the GameObject if not already added
-        audioSource = gameObject.AddComponent<AudioSource>();
 
         // Configure the AudioSource for looping
         audioSource.loop = true;
@@ -24,7 +23,6 @@ public class PlaySoundLoopSimple : MonoBehaviour
 
         // Assign the clip to the AudioSource and play
         audioSource.clip = clip;
-        audioSource.volume = 0.15f;
         audioSource.Play();
     }
 
